fa05b93b4f4a8d818365ad814fa9940f823aa40d
Rajan Kushwaha <rajank@celcomsolutions.com>
2023-05-12 14:44:57 +0530

    class came changes

--- src/main/java/com/entoss/pomfret/esb/KanikoFiataEfblApplication.java
+++ src/main/java/com/entoss/pomfret/esb/KanikoFiataEfblApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FiataEfblApplication {
+public class KanikoFiataEfblApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(FiataEfblApplication.class, args);
+		SpringApplication.run(KanikoFiataEfblApplication.class, args);
 	}
 }
